c62b1963a67e0be5881ba606946855a537254ad3
Andrew Johnston <av.johnst@gmail.com>
2020-12-03 09:17:44 -0700

    Fix the attr_reader

--- lib/school.rb
+++ lib/school.rb
@@ -1,14 +1,9 @@
 class School
-  attr_reader :start_time,
-              :hours_in_school_day
-              :student_names
+  attr_reader :start_time, :hours_in_school_day, :student_names
+
   def initialize(start_time, hours_in_school_day)
     @start_time = start_time
     @hours_in_school_day = hours_in_school_day
-    @student_names = []
-  end
-
-  def student_names
     @student_names = []
   end
 
